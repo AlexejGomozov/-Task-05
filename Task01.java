3cda0b45004cb9c4a442923deb88b6aec21fd5b4
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-25 09:03:56 +0200

    Update Task01.java

--- Task01.java
+++ Task01.java
@@ -1,31 +1,15 @@
-package by.htp.onedimensionalarrays.logic;
-//import java.util.Arrays.*;
-/* В массив A [N] занесены натуральные числа. Найти сумму тех элементов,
- *  которые кратны данному К. */
-public class Task01 {
+package by.htp.onedimensionalarrays.logic;  
+/* В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+ The array A [N] contains natural numbers. Find the sum of those elements that are multiples of a given K. */
+   public class Task01 {
 	
-	/*public static int divAmo(int []x, int k) {
-		 k = 2;
-		int sum = 0;
-		for( int i: x) {
-			if (x[i]% k == 0) sum = sum + x[i];
-			
-		}
-		return sum;
-	}
-	*/
-	
-	public static void main(String[] args) {          //public static void task() {
+      public static void main(String[] args) {         
 	int K = 2;
-	int sum = 0;
-	int [] A = {1, 10, 55, 39, 97, 345, 98, 0, 57, 9};     //для нахождения значения ячеек массива можно использовать Scanner or random 
-	for( int i=0; i < A.length; i++) {
-		if (A[i]% K == 0) {sum = sum + A[i];}
-		//else continue;
-		
-	
-	//System.out.print( divAmo(A, K));
-}
-	System.out.print( sum);
-	}
-}
+	  int sum = 0;
+	     int [] A = {1, 10, 55, 39, 97, 345, 98, 0, 57, 9};     //для нахождения значения ячеек массива можно использовать Scanner or random 
+	       for( int i=0; i < A.length; i++) {
+		 if (A[i]% K == 0) {sum = sum + A[i];}			
+	           }
+	            System.out.print( sum);
+	              }
+                        }
