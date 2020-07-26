d4e45e33f9d8823540163cdf2935baaad82e81f4
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-26 13:48:10 +0200

    Update Task07.java

--- Task07.java
+++ Task07.java
@@ -11,31 +11,33 @@ import java.util.Arrays;
 
 	  public static int counter (int [] x, int c) {   //метод для подсчета замен на опред число 
 	
-	    int count = 0;
+	     int count = 0;
 	
-	      for(int i = 0; i < x.length; i++) {
+	       for(int i = 0; i < x.length; i++) {
 		
-		     if(x[i] > c) count++; 
-	          }
+		    if(x[i] > c) count++; 
+	              }
 		       return  count;
-	            }	   	
+	                }	   	
 	
 	    public static int [] replaceForSpecificNumber(int [] x, int c) {   // метод - замена чисел
 			
-		   for(int i = 0; i < x.length; i++) {
+		  for(int i = 0; i < x.length; i++) {
 		
-	         if(x[i] > c)  x[i] = c; 
-		       }
-		        return x ;
-	             }
-	                      public static void main(String [] args) {
+	                if(x[i] > c)  x[i] = c; 
+		          }
+		           return x ;
+	                      }
+	                                    public static void main(String [] args) {
 		
-		                     int [] b = {12, 37, 86, 56, 75, 64, 720, 91, 904, 7};
+		                                 int [] b = {12, 37, 86, 56, 75, 64, 720, 91, 904, 7};
 		
-		                        int Z = 20;
+		                                       int Z = 20;
 		
-		                           System.out.println(counter(b,Z) + " - количество замен. " );
+		                                            System.out.println(counter(b,Z) + " - количество замен. " );
 		
-		                              System.out.print(Arrays.toString(replaceForSpecificNumber(b,Z)));
-	                                   }
-                                         }
+		                                                System.out.print(Arrays.toString(replaceForSpecificNumber(b,Z)));
+	                                                            }
+                                                                       }
+                                           //8 - количество замен. 
+                                           //[12, 20, 20, 20, 20, 20, 20, 20, 20, 7]
