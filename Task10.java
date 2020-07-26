b9f78d4df6b303b0e43e13e30fec7c66a8ab9605
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-26 16:15:59 +0200

    Update Task10.java

--- Task10.java
+++ Task10.java
@@ -1,45 +1,47 @@
 package by.htp.onedimensionalarrays.logic;
 import java.util.Arrays;
 
-/* Даны целые числа а1 ,а2 ,..., аn . Вывести на печать только те числа, для которых аi > i. */
+/* Даны целые числа а1 ,а2 ,..., аn . Вывести на печать только те числа, для которых аi > i.
+   Integers a1, a2, ..., an are given. Print only those numbers for which ai> i.*/
 
-public class Task10 {
+   public class Task10 {
 
-	public static int cellСounter(int[]x){      //счетчик ячеек для массива
+       public static int cellСounter(int[]x){                                   //счетчик ячеек для массива
 		
-		int count = 0;
+           int count = 0;
 		
 		for(int i = 0; i< x.length; i++) {
 			
-			if(x[i] > i) count++;
-		}
+		    if(x[i] > i) count++;
+		      }
 		
-		return count;
-	}
+		        return count;
+	                 }
 	
-  public static int[]higherOrderNumbers(int[] x) {
+                                  public static int[]higherOrderNumbers(int[] x) {
 			
-	int [] printNeededNumbers = new int [cellСounter(x)];
+	                              int [] printNeededNumbers = new int [cellСounter(x)];
 	
-	int j = 0;
+	                                 int j = 0;
 	
-		for(int i = 0; i< x.length; i++) {
+		                            for(int i = 0; i< x.length; i++) {
 			
-			if(x[i] > i)  {
+			                      if(x[i] > i)  {
 				
-				if(j == cellСounter(x)) break;   //если, при этом цикле, номер ячейки нового массива станет равным 
-				                                  // счетчику ячеек для массива, то прекращаем цикл
-				printNeededNumbers[j] = x[i];  j++;
-			}
-		}
+				                 if(j == cellСounter(x)) break;               //если, при этом цикле, номер ячейки нового массива станет равным 
+				                                                              // счетчику ячеек для массива, то прекращаем цикл
+				                    printNeededNumbers[j] = x[i];  j++;
+			                              }
+		                                        }
 		
-		return printNeededNumbers;
-}
+		                                         return printNeededNumbers;
+                                                           }
 	
-	public static void main(String[]args) {
+	                                                public static void main(String[]args) {
 		
-		int [] c = {0, 1, 2, 3, 7, 9, 0, 8, 7, 8, 6, 56, 55, 11, 23};
+		                            int [] c = {0, 1, 2, 3, 7, 9, 0, 8, 7, 8, 6, 56, 55, 11, 23};
 		
-		System.out.print(Arrays.toString(higherOrderNumbers(c)));
-	}
-}
+		              System.out.print(Arrays.toString(higherOrderNumbers(c)));
+	                 }
+                   }
+                     //[7, 9, 8, 56, 55, 23]
