dee6d6217d3546cbfcbcf28f825925627c144554
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-26 14:13:08 +0200

    Update Task07.java

--- Task07.java
+++ Task07.java
@@ -7,37 +7,42 @@ import java.util.Arrays;
     A sequence of real numbers a1, a2, ..., an is given. Replace all its members greater 
     than the given Z with this number. Count the number of substitutions. */
 
-    public class Task07 {
+    public class Task07 {         //можно решить двумя способами через два метода или один (void)
 
-	  public static int counter (int [] x, int c) {   //метод для подсчета замен на опред число 
+	/*  public static int counter (int [] x, int c) {       //метод для подсчета замен на опред число 
 	
-	     int count = 0;
+	       int count = 0;                                  
 	
-	       for(int i = 0; i < x.length; i++) {
+	          for(int i = 0; i < x.length; i++) {
 		
-		    if(x[i] > c) count++; 
-	              }
+		     if(x[i] > c) count++; 
+	               }
 		       return  count;
-	                }	   	
-	
-	    public static int [] replaceForSpecificNumber(int [] x, int c) {   // метод - замена чисел
-			
-		  for(int i = 0; i < x.length; i++) {
+	                 }	   	
+	*/
+	             public static void replaceForSpecificNumber(int [] x, int c) {   // метод - замена чисел
+
+		         int count = 0;
+	    	
+		            for(int i = 0; i < x.length; i++) {
 		
-	                if(x[i] > c)  x[i] = c; 
-		          }
-		           return x ;
-	                      }
-	                                    public static void main(String [] args) {
+	                        if(x[i] > c)  {count++;  x[i] = c; }
+		                   }
+		                    System.out.println(count + " - количество замен. " );
+		                       System.out.print(Arrays.toString(x));
+	                                 }
+	                               public static void main(String [] args) {
 		
-		                                 int [] b = {12, 37, 86, 56, 75, 64, 720, 91, 904, 7};
+		                     int [] b = {12, 37, 86, 56, 75, 64, 720, 91, 904, 7};
 		
-		                                       int Z = 20;
+		                  int Z = 20;
 		
-		                                            System.out.println(counter(b,Z) + " - количество замен. " );
+		             replaceForSpecificNumber(b, Z);
+		                        
+		                             //  System.out.println(counter(b,Z) + " - количество замен. " );
 		
-		                                                System.out.print(Arrays.toString(replaceForSpecificNumber(b,Z)));
-	                                                            }
-                                                                       }
-                                           //8 - количество замен. 
-                                           //[12, 20, 20, 20, 20, 20, 20, 20, 20, 7]
+		                             // System.out.print(Arrays.toString(replaceForSpecificNumber(b,Z)));
+	                 }
+                    }
+           //    8 - количество замен. 
+          //    [12, 20, 20, 20, 20, 20, 20, 20, 20, 7]
