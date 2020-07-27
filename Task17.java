b2f5d1d6c758e9bab9c46d1b942e7897edbedec8
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-27 12:53:05 +0200

    Update Task17.java

--- Task17.java
+++ Task17.java
@@ -1,57 +1,58 @@
 package by.htp.onedimensionalarrays.logic;
 import java.util.Arrays;
 /* Дана последовательность целых чисел a1,a2,..,an . Образовать новую последовательность, 
-   выбросив из исходной те члены, которые равны min(a1,a2,...,an). */
+   выбросив из исходной те члены, которые равны min(a1,a2,...,an).
+      Given a sequence of integers a1, a2,.., an . Form a new sequence by throwing out
+      the original terms that are equal to min (a1,a2,...,an).*/
 
-public class Task17 {
+  public class Task17 {
 	
-public static int findMinimum (int [] x) {
+       public static int findMinimum (int [] x) {
 		
-		int min = x[0];
+	   int min = x[0];
 		
 		for (int i = 1; i < x.length; i++) {
 			
-			min = Math.min(min, x[i]);
-		}
+	            min = Math.min(min, x[i]);
+		       }
 		
-		return min;
-	}
+		         return min;
+	                   }
 	
-public static int quantityWithoutMinimum (int [] x) {
+                              public static int quantityWithoutMinimum (int [] x) {
 
-	int count = 0;
+	                         int count = 0;
 	
-	for (int i = 0; i < x.length; i++) {
+	                              for (int i = 0; i < x.length; i++) {
 		
-		if(x[i] == findMinimum(x)) count ++;
-	}
+		                          if(x[i] == findMinimum(x)) count ++;
+	                                    }
 	
-	return x.length - count;
-}
+	                                      return x.length - count;
+                                                }
 
-	public static int [] sequenceWithoutMinimum (int [] x) {
+	                                           public static int [] sequenceWithoutMinimum (int [] x) {
 		
-		int [] sequence = new int [quantityWithoutMinimum(x)];
+		                                        int [] sequence = new int [quantityWithoutMinimum(x)];
 		
-		int j = 0;
+		                                            int j = 0;
 		
-		for (int i = 0; i < x.length; i++) {
+		                                                for (int i = 0; i < x.length; i++) {
 			
-			if(x[i] != findMinimum(x)) {
+			                                            if(x[i] != findMinimum(x)) {
 				
-				if (j == quantityWithoutMinimum(x)) break; sequence[j] = x[i]; j++;
-				}
-		}
+				                                        if (j == quantityWithoutMinimum(x)) break; sequence[j] = x[i]; j++;
+				                                           }
+		                                                             }
 		
-		return sequence;
-	}
+		                                                             return sequence;
+										 }
 	
-	public static void main(String[] args) {
+									     public static void main(String[] args) {
 		
-		int [] y = {4, 7, 4, 8, 90, 78, 50, 4, 34, 87, 20, 81};
+								    int [] y = {4, 7, 4, 8, 90, 78, 50, 4, 34, 87, 20, 81};
 		
-		System.out.print(Arrays.toString(sequenceWithoutMinimum(y)));
-	}
-
-}
-        //[7, 8, 90, 78, 50, 34, 87, 20, 81]
+							    System.out.print(Arrays.toString(sequenceWithoutMinimum(y)));
+						    }
+					     }
+                                                                             //[7, 8, 90, 78, 50, 34, 87, 20, 81]
