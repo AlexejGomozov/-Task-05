983a78b4eedb9a9f9822a3188791478240760e13
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-27 14:53:52 +0200

    Update Task18.java

--- Task18.java
+++ Task18.java
@@ -8,7 +8,7 @@ package by.htp.onedimensionalarrays.logic;
 
         public class Task18 {
 
-	    public static int thirdСell () {   
+	    public static int thirdСell () {                                        //not done yet
 		
 		int third;
 		
