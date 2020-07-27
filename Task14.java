231921e007f5542ccf2779e05911b37b9097367e
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-27 10:35:06 +0200

    Update Task14.java

--- Task14.java
+++ Task14.java
@@ -6,40 +6,40 @@ import java.util.Arrays;
 
 public class Task14 {
 
-public static  int counter (int []x, int v, int c) {   //счетчик ячеек для будущего массива
+   public static  int counter (int []x, int v, int c) {   //счетчик ячеек для будущего массива
 	                                                   //дан массив и точки между которыми нужно считать элементы
-	          int count = 0;                                 
+	int count = 0;                                 
 		
-		for(int i = v; i<= c; i ++ ) count ++;
-		
-		
+            for(int i = v; i<= c; i ++ ) count ++;
+				
 		return count;
-	}
+	          }
 	
-	public static  int[] numInSegment (int []y, int b, int n) {
+	            public static  int[] numInSegment (int []y, int b, int n) {
 		
-		int [] numbers = new int [counter(y, b, n)];
+		        int [] numbers = new int [counter(y, b, n)];
 		
-		int i = 0;
+		           int i = 0;
 		
-		for (int j = b; j <= n; j ++) { 
+		               for (int j = b; j <= n; j ++) { 
 			
-			numbers[i] = y[j]; i ++;			
-		}	
+			           numbers[i] = y[j]; i ++;			
+		                     }	
 		
-		return numbers;
+		                       return numbers;
 		
-	}
+	                                  }
 	
-	public static void main(String[] args) {
-		int a = 2;
+	                                     public static void main(String[] args) {
+						     
+		                                 int a = 2;
 		
-		int b = 15;
+		                                     int b = 15;
 		
-		int [] d = {85, 15, 62, 87, 36, 45, 17, 51, 97, 51, 98, 75, 17, 65, 19, 75, 91, 8, 75};
-		 
-		System.out.print(Arrays.toString(numInSegment(d, a, b)));
+		                                          int [] d = {85, 15, 62, 87, 36, 45, 17, 51, 97, 51, 98, 75, 17, 65, 19, 75, 91, 8, 75};
+		    
+		                                             System.out.print(Arrays.toString(numInSegment(d, a, b)));
 		
-	}
-}
-   //[62, 87, 36, 45, 17, 51, 97, 51, 98, 75, 17, 65, 19, 75]
+	                                                          }
+								      }
+                                                                           //[62, 87, 36, 45, 17, 51, 97, 51, 98, 75, 17, 65, 19, 75]
