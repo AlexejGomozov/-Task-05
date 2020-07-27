c30ba04b216431b92614283d32e306d5734c78ab
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-27 11:53:27 +0200

    Update Task16.java

--- Task16.java
+++ Task16.java
@@ -5,29 +5,26 @@ package by.htp.onedimensionalarrays.logic;
     You are given real numbers a1, a2, ..., an. To find
     max (a1 + a2n, a2 + a2n − 1, ..., an + an + 1). */
 
-public class Task16 {
+    public class Task16 {
 	
 	public static int maxSum(int [] x) {     //метод по нахождению max сумм первого и последнего ячеек, второй и предпоследней и т.д. 
 		
-		int max = x[0] + x[x.length-1];
+	    int max = x[0] + x[x.length-1];
 		
 		for(int i = 1; i <= x.length-1 - i; i++) {
 			
-			max = Math.max(max, x[i] + x[x.length - 1 - i]);
+	            max = Math.max(max, x[i] + x[x.length - 1 - i]);
 			
-		}
+		      }
 		
-		return max;
-	}
-	
-	
-	public static void main(String[] args) {
+		         return max;
+	                   }
 		
-		int [] y = {36, 4555, 68, 37, 450, 28, 73, 45, 87, 36, 480, 27, 50, 82, 73, 65, 23, 750};
+	                     public static void main(String[] args) {
 		
-		
-		System.out.print(maxSum(y));
-	}
-
-}
-     // 4578
+		                int [] y = {36, 4555, 68, 37, 450, 28, 73, 45, 87, 36, 480, 27, 50, 82, 73, 65, 23, 750};
+				
+		                   System.out.print(maxSum(y));
+	                              }
+                                         }
+                                            // 4578
