f3ce34d1250034261715577bb554789cc5b340fd
AlexejGomozov <65343579+AlexejGomozov@users.noreply.github.com>
2020-07-30 19:40:57 +0200

    Add files via upload

---
+++ Task20.java
@@ -0,0 +1,30 @@
+package by.htp.onedimensionalarrays.logic;
+import java.util.Arrays;
+
+/* Дан целочисленный массив с количеством элементов п. Сжать массив, выбросив из него каждый 
+ * второй элемент (освободившиеся элементы заполнить нулями).
+ *  Примечание. Дополнительный массив не использовать. 
+      An integer array is given with the number of elements n. Compress the array, throwing out
+      every second element from it (fill the freed elements with zeros). Note. Do not use
+      an additional array. */
+
+public class Task20 {
+
+	public static int []removeZeros(int[] k){
+		
+		for(int i = 1; i< k.length; i=i+2) {
+			
+			k[i] = 0;
+			
+		}
+		
+		return  k;
+	}
+		
+	public static void main(String[] args) {
+		
+		int [] x = {76, 8, 76, 2, 83, 4, 75, 2, 50, 18, 57, 38, 66, 78, 37, 48, 5, 20};
+		
+		System.out.print(Arrays.toString(removeZeros(x)));		
+	}
+}
